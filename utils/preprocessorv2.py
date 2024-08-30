2a198246545de5639f82aad85113e02719425c1e
Heical Chandra <haical31@gmail.com>
2024-08-30 16:16:05 +0700

    Update preprocessorv2.py

--- utils/preprocessorv2.py
+++ utils/preprocessorv2.py
@@ -23,7 +23,7 @@ class DataModule(pl.LightningDataModule):
         self.one_hot_label = one_hot_label
         self.train_dataset_path = "datasets/mendaley/github_tbh_train.json"
         self.validation_dataset_path = "datasets/mendaley/valid_data.json"
-        self.test_dataset_path = "datasets/github/mendaley_sentenced_test.json"
+        self.test_dataset_path = "datasets/mendaley/mendaley_sentenced_test.json"
         self.processed_dataset_path = "datasets/manual_processed.json"
 
     def load_data(self):
